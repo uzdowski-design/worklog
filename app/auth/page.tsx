70c6999a2b2986870f395e2fe1a27074d0ae00db
Michal Uzdowski <uzdowski.design@gmail.com>
2024-07-28 01:34:07 +0200

    fix build issues

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -3,6 +3,8 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Page() {
   const supabase = createServerComponentClient({ cookies });
   const { data } = await supabase.auth.getSession();
--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,8 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies });
   const { data } = await supabase.auth.getSession();
