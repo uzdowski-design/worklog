de808478f81731f0ffd48ab28b8896e7c2b18e30
Michal Uzdowski <uzdowski.design@gmail.com>
2024-10-28 16:16:10 +0100

    fixed login buttons

--- app/auth/components/AuthComponent.tsx
+++ app/auth/components/AuthComponent.tsx
@@ -27,15 +27,17 @@ export default function AuthComponent() {
     <div className="p-5 flex flex-col flex-grow">
       <Navbar />
       <div className="flex justify-center items-center flex-grow">
-        <div className="text-center space-y-5">
+        <div className="text-center space-y-10">
           <p className="italic">
             Remember, &ldquo;time is money&rdquo;.
             <br />
             Invest it wisely with <span className="font-bold">WorkLog </span>
             app.
           </p>
-          <Button onClick={handleGoogleLogin}>Login with Google</Button>
-          <Button onClick={handleGithubLogin}>Login with GitHub</Button>
+          <div className="flex flex-col gap-4">
+            <Button onClick={handleGoogleLogin}>Login with Google</Button>
+            <Button onClick={handleGithubLogin}>Login with GitHub</Button>
+          </div>
         </div>
       </div>
     </div>
